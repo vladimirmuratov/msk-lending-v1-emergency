7488d8f6adf5aa22becc37ef724d9ec8e7e9d49d
vladimir <vladimirmuratov@mail.ru>
2026-01-31 18:41:21 +0300

    update canonical

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,20 +12,23 @@ import './globals.css'
 const montserrat = Montserrat({subsets: ['latin', 'cyrillic']})
 
 export const metadata = {
-    metadataBase: new URL('https://emergency.mskdoctor.ru/'),
+    metadataBase: new URL('https://msk-aid.vercel.app'),
     title: 'Экстренная госпитализация — срочная медицинская помощь 24/7',
     description: 'Экстренная госпитализация при острых состояниях. Быстрое размещение в стационаре, круглосуточный приём, медицинское сопровождение. Обращайтесь сразу.',
     keywords: ['платная экстренная госпитализация', 'экстренная платная госпитализация в москве', 'экстренная госпитализация в стационар платно'],
     other: {
-        ['yandex-verification']: '643910354c1963f8',
+        ['yandex-verification']: '8bc5d33e337d5fd2',
     },
     openGraph: {
         title: 'Экстренная госпитализация. 8 (499) 719-81-00, 24/7',
         description: 'Срочная госпитализация в стационар без очередей. Принимаем 24/7, обеспечим неотложную помощь и полное медицинское сопровождение. Опытные врачи, комфортные условия.',
         images: ['/images/banner2.webp'],
-        url: 'https://emergency.mskdoctor.ru/',
+        url: 'https://msk-aid.vercel.app/',
         type: 'website',
         locale: 'ru_RU',
+    },
+    alternates: {
+        canonical: 'https://msk-aid.vercel.app/',
     }
 };
 
